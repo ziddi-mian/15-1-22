d404b585b19a9930e53ec54c77db18a296bbcb3f
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-15 19:02:37 +0500

    jsjs

--- script.js
+++ script.js
@@ -225,7 +225,6 @@ console.log(diff_weeks(dt1, dt2));
 
 function diff_months(dt2, dt1) 
  {
-
   var diff =(dt2.getTime() - dt1.getTime()) / 1000;
    diff /= (60 * 60 * 24 * 7 * 4);
   return Math.abs(Math.round(diff));
