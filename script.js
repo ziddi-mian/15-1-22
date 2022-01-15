44fba5f521e06cc498ab68179fd26f7914a319a1
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-15 18:58:18 +0500

    js

--- script.js
+++ script.js
@@ -176,7 +176,6 @@ console.log(diff_hours(dt1, dt2));
 
 function diff_days(dt2, dt1) 
  {
-
   var diff =(dt2.getTime() - dt1.getTime()) / 1000;
   diff /= (60 * 60 * 24);
   return Math.abs(Math.round(diff));
