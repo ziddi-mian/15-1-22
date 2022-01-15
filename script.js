1984cb1c42ac82c867ebc138b891facbc7ded78d
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-15 18:46:59 +0500

    js

--- script.js
+++ script.js
@@ -62,7 +62,6 @@ function timezone_offset_in_seconds(dt)
 dt = new Date(); 
 console.log(timezone_offset_in_seconds(dt)); 
 
-
 dt = new Date(1989, 10, 1); 
 console.log(timezone_offset_in_seconds(dt));
 
