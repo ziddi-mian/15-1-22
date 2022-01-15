68eccea80ca753f0ed445fad9965864a28f390eb
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-15 18:55:37 +0500

    js

--- script.js
+++ script.js
@@ -162,7 +162,6 @@ dt1 = new Date(2014,10,2);
 dt2 = new Date(2014,10,3);
 console.log(diff_hours(dt1, dt2));
 
-
 dt1 = new Date("October 13, 2014 08:11:00");
 dt2 = new Date("October 13, 2014 11:13:00");
 console.log(diff_hours(dt1, dt2));
