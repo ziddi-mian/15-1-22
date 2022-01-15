f8764b9f74f1044ac10fad83924c7baa31762bf8
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-15 18:41:23 +0500

    jf

--- script.js
+++ script.js
@@ -41,7 +41,6 @@ function diff_to_GMT(dt)
     return (-dt.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(dt.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(dt.getTimezoneOffset() / 60)) + '00';
 }
 
-
 dt = new Date(); 
 console.log(diff_to_GMT(dt)); 
 
