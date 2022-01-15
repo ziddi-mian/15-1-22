22a84aece9dc8e3dd4fc04b5ff1d80d9f9fddc0c
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-15 19:04:14 +0500

    ugf

--- script.js
+++ script.js
@@ -262,7 +262,6 @@ function diff_years(dt2, dt1)
 dt1 = new Date(2014,10,2);
 dt2 = new Date(2014,10,11);
 console.log(diff_years(dt1, dt2));
-
 dt1 = new Date("June 13, 2014 08:11:00");
 dt2 = new Date("October 19, 2017 11:13:00");
 console.log(diff_years(dt1, dt2));
